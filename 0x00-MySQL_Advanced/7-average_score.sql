3f7b471c32d8780a01659d19d39354b697d6a3e6
Biitopher <biitopher@gmail.com>
2024-01-17 20:59:12 +0000

    Task 7

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,15 @@
+-- Assuming you have tables named 'users' and 'corrections'
+-- Create the ComputeAverageScoreForUser stored procedure
+
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+	UPDATE users
+	SET
+	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
+
+END //
+
+DELIMITER ;
