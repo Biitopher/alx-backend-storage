6ca38e0a7150c71424d065ec75761f94a3f657a5
Biitopher <biitopher@gmail.com>
2024-01-17 21:05:51 +0000

    Task 8

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,4 @@
 -- Assuming you have a table named 'names'
 -- Create the idx_name_first index on the first letter of the 'name' column
 
-CREATE INDEX idx_name_first ON names (SUBSTRING(name FROM 1 FOR 1));
+CREATE INDEX idx_name_first ON names(name(1));
