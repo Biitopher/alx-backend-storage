c7f9af341f9091837bc1b82095af68e646badb2f
Biitopher <biitopher@gmail.com>
2024-01-17 17:43:01 +0000

    Readme

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Check if the table already exists before creating
 -- Create the users table
-    CREATE IF NOT EXIST TABLE users (
+    CREATE TABLE IF NOT EXIST users (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,       
         email VARCHAR(255) NOT NULL UNIQUE,
         name VARCHAR(255),
