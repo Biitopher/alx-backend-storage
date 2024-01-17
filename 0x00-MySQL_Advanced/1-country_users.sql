fdb29d193aea8e2f379b6e1543d5b8975c7998e2
Biitopher <biitopher@gmail.com>
2024-01-17 17:57:07 +0000

    Task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- Check if the table already exists before creating
+-- Create the users table
+
+CREATE TABLE IF NOT EXISTS users (
+       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+       email VARCHAR(255) NOT NULL UNIQUE,
+       name VARCHAR(255),
+       country ENUM ('US', 'CO', 'TN') NOT NULL
+);
