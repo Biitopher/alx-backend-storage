a5a310c300822e1e72306b69a354f829b1ad3f56
Biitopher <biitopher@gmail.com>
2024-01-24 11:28:10 +0000

    Task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,3 +16,19 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> (
+            Union[str, bytes, int, float, None]):
+        """Get"""
+        data = self._redis.get(key)
+        if data is not None:
+            return fn(data) if fn else data
+        return None
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """Get string"""
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """Get int"""
+        return self.get(key, fn=int)
